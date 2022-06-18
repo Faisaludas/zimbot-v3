4e4cddb4f82779427facb9bd394425a351a2be20
Faisaludas <107762003+Faisaludas@users.noreply.github.com>
2022-06-18 22:00:51 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-
+welcome dear friend Faisal udas
 // ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
 //▮ZIM BOT INC 2022 ®️ALL RIGHTS RESERVED
 //▮
@@ -8,8 +8,8 @@
 //▮
 //▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
 //▮reinhardtuna@mail.uk
-//▮WHATSAPP US : +44 7441 437150
-//▮YOUTUBE CHANNELL: https://youtube.com/c/DRIPSOFC
+//▮WHATSAPP US : +923037484167
+//▮YOUTUBE CHANNELL: https://youtube.com/channel/UCbkbMM3bTOZDFeOuNI1tfwQ
 //▮
 //╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
 //
@@ -37,12 +37,12 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['923474187615']
-global.premium = ['923474187615']
-global.botname = '𝗛𝗮𝗰𝗸𝗲𝗿 𝗕𝗼𝘆•💙🥀'
-global.ownername ='ASIF'
-global.packname = '𝗛𝗮𝗰𝗸𝗲𝗿 𝗕𝗼𝘆•💙🥀'
-global.quoted = 'https://wa.me/+923474187615'
+global.owner = ['923037484167']
+global.premium = ['923037484167']
+global.botname = 'Faisal udas•💙🥀'
+global.ownername ='Faisal udas'
+global.packname = 'Faisal udas💙🥀'
+global.quoted = 'https://wa.me/+923037484167'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
@@ -57,12 +57,12 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = '𝗛𝗮𝗰𝗸𝗲𝗿 𝗕𝗼𝘆•💙🥀'
+global.author = 'Faisal udas•💙🥀'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
-global.sessionName = 'session'
+global.sessionName = 'Faisal udas
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
-global.mess = {
+global.mess = {Faisal udas
     success: '*success whoa*',
         ban: '*Sorry, looks like u got banned😔😔 type .owner To request unban okay*',
     admin: '*this cmd is for bot owner only , l can feel you but sorry okay*',
